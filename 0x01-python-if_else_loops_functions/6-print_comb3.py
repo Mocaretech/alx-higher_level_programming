7725b6e22935a9da8e322a39d18ced110be5a8f6
Jelil Usman <mocaretech@gmail.com>
2024-09-11 23:00:41 +0100

    printing possible  combination of digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for c in range(0, 10):
     for d in range(c+1, 10):
         if c == 8 and d == 9:
-            print("{}{}".format(c,d))
+            print("{}{}".format(c, d))
         else:
-            print("{}{},".format(c,d), end=" ")
+            print("{}{},".format(c, d), end=" ")
