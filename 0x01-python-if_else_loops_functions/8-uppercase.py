e11c1c9b87a7b0b6762e7f682d3562a93cd8c117
Jelil Usman <mocaretech@gmail.com>
2024-09-12 00:19:45 +0100

    printing upper case letters

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(str):
     for str in range(65, 90):
         if ord(str) >= 65 and ord(str) <= 90:
-            print (True)
+            print('{}'.format(True))
         else:
-            print (False)
+            print ('{}'.format(False))
