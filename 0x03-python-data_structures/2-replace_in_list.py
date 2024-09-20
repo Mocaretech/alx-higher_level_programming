7d479ca7655dc73f2d065c9e828ccb4881cfc016
Jelil Usman <mocaretech@gmail.com>
2024-09-20 12:46:51 +0100

    replacing of element in a list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    my_list[idx] = element
     if idx < 0:
         return(my_list)
-    elif idx > len(my_list):
+    if idx > len(my_list):
         return(my_list)
     my_list[idx] = element
-    return(my_list)
+    for items in my_list:
+        print(items)
+
