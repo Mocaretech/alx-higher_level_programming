a036fd4dee324054a2dc19f990c051d18d66b7ae
Jelil Usman <mocaretech@gmail.com>
2024-09-20 13:14:53 +0100

    replacing of element in a list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list = []
-    reverse_list = my_list.reverse()
-    print('{:d}'.format(reverse_list))
+    if my_list:
+        my_list.reverse()
+        for figures in my_list:
+            print('{:d}'.format(figures))
