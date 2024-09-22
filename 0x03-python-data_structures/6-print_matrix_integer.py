e7edfecb5dab335a052d115071a16bf6783a791f
Jelil Usman <mocaretech@gmail.com>
2024-09-22 20:09:27 +0100

    Printing matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    import numpy as matrix
+    matrix = nump.array([
+        [2,4,5],
+        [1,2,4],
+        [4,9,2]
+
+])
+    print('{}'.format(matrix))
