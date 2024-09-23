277d2b70c0bb7f765bee5a716b382a97eb4d5229
Jelil Usman <mocaretech@gmail.com>
2024-09-23 10:48:06 +0100

    return length of tuple and first letter

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    sentence = ("My name is Jelil")
+    for char in sentence:
+        print("length:{:d} - First character {}".format(len(char), char[0]))
