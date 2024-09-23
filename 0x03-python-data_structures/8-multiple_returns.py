8ce5d8b78c7711d35d70ce4f20455878b8994c5f
Jelil Usman <mocaretech@gmail.com>
2024-09-23 10:54:29 +0100

    return length of tuple and first letter

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     for char in sentence:
+        if sentence = "":
+            return(0, None)
         print("length:{:d} - First character {}".format(len(char), char[0]))
