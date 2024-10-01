acca55cd100777465e940ac437794c08a376dee8
Jelil Usman <mocaretech@gmail.com>
2024-10-01 15:41:39 +0100

    Type error

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    value = input("please enter the value")
+    try:
+        print("{:d}".format(value))
+        return(True)
+    except TypeError:
+        print("Opp, this is not an integer")
+        return(False)
