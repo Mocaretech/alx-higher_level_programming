87d68bd9654979dcf37097b184a1b4af41c33859
Jelil Usman <mocaretech@gmail.com>
2024-10-02 01:37:34 +0100

    printing with ZeroDivisionError

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        solution = a / b
+        result = a / b
     except ZeroDivisionError:
-        solution = None
+        result = None
     finally:
-        print("inside result: {}".format(solution))
-    return(solution)
+        print("{:d} / {:d} = inside result: {}".format(a, b, result))
+    return(result)
