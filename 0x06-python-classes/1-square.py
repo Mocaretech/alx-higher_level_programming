b910d2e3feda3cdbde8588f6c097068e5cbb0e2f
Jelil Usman <mocaretech@gmail.com>
2024-10-10 03:54:35 +0100

    size of variable

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,5 +2,10 @@
 class Square:
     def __init__(self, size):
         self._size = size
+
+
 my_square = Square(1)
-print(my_square._size)
+try:
+    print(my_square._size)
+except TypeError:
+    return(None)
